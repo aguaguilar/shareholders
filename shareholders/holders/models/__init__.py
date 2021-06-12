28e7260426e08c0bc11344443e3a5c965c1faa76
Agustin AguilAR <aguaguilar.ar@gmail.com>
2021-06-11 23:23:47 -0300

    move models to models folder

---
+++ shareholders/holders/models/__init__.py
@@ -0,0 +1,8 @@
+from .write_only import Person, Organization, Share
+
+
+__all__ = [
+    'Organization',
+    'Person',
+    'Share',
+]
---
+++ shareholders/holders/models/write_only.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.db.models import Q
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=255)
+    birth_date = models.DateField()
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=255)
+    orgnr = models.IntegerField(primary_key=True)
+
+
+class Share(models.Model):
+    SHARE_CLASS_CHOICES = [
+        ("A-aksjer", "A-aksjer"),
+        ("B-aksje", "B-aksje"),
+    ]
+
+    organization_owner = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name="organization_owned_shares",
+        null=True,
+    )
+    person_owner = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+        related_name="person_owned_shares",
+        null=True,
+    )
+    organization_owned = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name="owner_shares"
+    )
+    share_class = models.CharField(choices=SHARE_CLASS_CHOICES, max_length=8)
+    amount = models.IntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=(
+                              Q(organization_owner__isnull=False) &
+                              Q(person_owner__isnull=True)
+                      ) | (
+                              Q(organization_owner__isnull=True) &
+                              Q(person_owner__isnull=False)
+                      ),
+                name='only_one_kind_of_owner',
+            )
+        ]
