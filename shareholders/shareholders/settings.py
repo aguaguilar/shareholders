44eabbc1a935db9ff96c8d420e31c4353b2d458c
Agustin AguilAR <aguaguilar.ar@gmail.com>
2021-06-12 00:29:59 -0300

    installing rest_framework

--- shareholders/shareholders/settings.py
+++ shareholders/shareholders/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iid%@ad)nezt2#z!!2!l==hzhc#375ezur!*1(()v48ic!qdoy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'holders',
 ]
 
