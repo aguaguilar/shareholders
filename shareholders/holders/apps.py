cfe9b9ccc84fbd361b5dfb9b8afba016c6e88c6e
Agustin AguilAR <aguaguilar.ar@gmail.com>
2021-06-13 14:46:57 -0300

    add signal

--- shareholders/holders/apps.py
+++ shareholders/holders/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class HoldersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'holders'
+
+    class Meta:
+        app_label = 'holders'
+
+    def ready(self):
+        from .signals import save_in_read_only_db  # noqa
