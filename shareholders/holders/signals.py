fc7dca0bfae8e8d40363618a8ad9b4e3a61bbbee
Agustin AguilAR <aguaguilar.ar@gmail.com>
2021-06-13 17:11:54 -0300

    save in ro db async

--- shareholders/holders/signals.py
+++ shareholders/holders/signals.py
@@ -1,6 +1,9 @@
+import os
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Share
+from .amqp import Amqp
 
 
 @receiver(post_save, sender=Share)
@@ -8,4 +11,8 @@ def save_in_read_only_db(sender, instance, *args, **kwargs):
     """
     After save it into write-only database (a.k.a psql) we have to save it into read-only db as well.
     """
-    instance.save_in_ro_db()
+    if os.getenv("WRITE_READ_ONLY_ASYNC"):
+        amqp = Amqp()
+        amqp.publish(str(instance.id))
+    else:
+        instance.save_in_ro_db()
