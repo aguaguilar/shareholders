5a9644d81bcbf8bff781b874a2beaf3b2794a8c5
Agustin AguilAR <aguaguilar.ar@gmail.com>
2021-06-13 14:48:48 -0300

    serve data trough endpoints

--- shareholders/holders/urls.py
+++ shareholders/holders/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 
-from .views import OrganizationViewSet, PersonViewSet, ShareViewSet
+from .views import (
+    OrganizationViewSet,
+    PersonViewSet,
+    ShareViewSet,
+    ShareOwnersViewSet,
+    ShareHoldingViewSet,
+    OrganizationSummaryViewSet
+)
 
 
 urlpatterns = [
     path('organization/', OrganizationViewSet.as_view()),
     path('person/', PersonViewSet.as_view()),
     path('share/', ShareViewSet.as_view()),
+    path('<int:orgnr>/owners', ShareOwnersViewSet.as_view()),
+    path('<int:orgnr>/holding', ShareHoldingViewSet.as_view()),
+    path('<int:orgnr>/summary', OrganizationSummaryViewSet.as_view()),
 ]
